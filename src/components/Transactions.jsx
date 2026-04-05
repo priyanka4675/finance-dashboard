bc469547969006470d808b1693720e908cd38da3
priyanka4675 <priyanka.pilla999@gmail.com>
2026-04-05 15:34:04 +0530

    Transactions.jsx

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -2,33 +2,38 @@ import React, { useState } from "react";
 
 export default function Transactions({ data }) {
   const [search, setSearch] = useState("");
+  const [typeFilter, setTypeFilter] = useState("all");
 
-  // filter logic
-  const filteredData = data.filter((t) =>
-    t.category.toLowerCase().includes(search.toLowerCase())
-  );
+  const filteredData = data.filter((t) => {
+    return (
+      t.category.toLowerCase().includes(search.toLowerCase()) &&
+      (typeFilter === "all" || t.type === typeFilter)
+    );
+  });
 
   return (
     <div>
       <h2>Transactions</h2>
 
-      {/* 🔍 SEARCH BOX */}
       <input
         type="text"
         placeholder="Search category..."
         value={search}
         onChange={(e) => setSearch(e.target.value)}
-        style={{ padding: "5px", marginBottom: "10px" }}
       />
 
-      {/* LIST */}
+      <select onChange={(e) => setTypeFilter(e.target.value)}>
+        <option value="all">All</option>
+        <option value="income">Income</option>
+        <option value="expense">Expense</option>
+      </select>
+
       {filteredData.map((t) => (
         <div key={t.id}>
           {t.date} | ₹{t.amount} | {t.category} | {t.type}
         </div>
       ))}
 
-      {/* EMPTY CASE */}
       {filteredData.length === 0 && <p>No transactions found</p>}
     </div>
   );
