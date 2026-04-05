b558913bb8851d376e3ca3bfd5bcbe51c2d344dc
priyanka4675 <priyanka.pilla999@gmail.com>
2026-04-05 22:25:58 +0530

    SummaryCard.jsx

--- src/components/SummaryCard.jsx
+++ src/components/SummaryCard.jsx
@@ -1,8 +1,10 @@
+import React from "react";
+
 export default function SummaryCard({ title, value }) {
   return (
-    <div style={{ border: "1px solid gray", padding: "10px", borderRadius: "8px" }}>
+    <div>
       <h3>{title}</h3>
-      <h2>₹ {value}</h2>
+      <p>₹ {value}</p>
     </div>
   );
 }
