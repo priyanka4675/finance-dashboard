9e388f3d79286f46fe498fcfbb0c64024e5688a0
priyanka4675 <priyanka.pilla999@gmail.com>
2026-04-05 14:24:37 +0530

    App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { data } from "./data/data";
+import SummaryCard from "./components/SummaryCard";
+import Transactions from "./components/Transactions";
+import Insights from "./components/Insights";
+import RoleToggle from "./components/RoleToggle";
+
+function App() {
+  const [role, setRole] = useState("viewer");
+
+  const income = data.filter(d => d.type === "income")
+    .reduce((a, b) => a + b.amount, 0);
+
+  const expense = data.filter(d => d.type === "expense")
+    .reduce((a, b) => a + b.amount, 0);
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Rajeev Finance Dashboard</h1>
+
+      <RoleToggle role={role} setRole={setRole} />
+
+      <div style={{ display: "flex", gap: "10px" }}>
+        <SummaryCard title="Income" value={income} />
+        <SummaryCard title="Expense" value={expense} />
+        <SummaryCard title="Balance" value={income - expense} />
+      </div>
+
+      {role === "admin" && <button>Add Transaction</button>}
+
+      <Insights data={data} />
+      <Transactions data={data} />
+    </div>
+  );
+}
+
+export default App;
