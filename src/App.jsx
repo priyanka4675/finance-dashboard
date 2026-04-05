fdd8fc47f1c5e1787c7082a997a991555f54929c
priyanka4675 <priyanka.pilla999@gmail.com>
2026-04-05 15:19:45 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,8 @@
-import React, { useState } from "react";
-
 function App() {
-  const [role, setRole] = useState("viewer");
-
   return (
     <div style={{ padding: "20px" }}>
       <h1>Priyanka Finance Dashboard</h1>
-
-      <select value={role} onChange={(e) => setRole(e.target.value)}>
-        <option value="viewer">Viewer</option>
-        <option value="admin">Admin</option>
-      </select>
-
-      <p>Dashboard is working ✅</p>
+      <p>Now working properly ✅</p>
     </div>
   );
 }
