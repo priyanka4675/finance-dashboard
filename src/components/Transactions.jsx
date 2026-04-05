f574b0aec46381de101e45dd48d5994fc5b258e6
priyanka4675 <priyanka.pilla999@gmail.com>
2026-04-05 22:47:31 +0530

    Transactions.jsx

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 export default function Transactions({ data }) {
   const [search, setSearch] = useState("");
   const [typeFilter, setTypeFilter] = useState("all");
